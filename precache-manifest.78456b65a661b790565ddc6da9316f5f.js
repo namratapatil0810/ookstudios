214b5d702d8757c10d95485afe7a9931c9d10def
Ashutosh Telang <ashutoshtel@gmail.com>
2018-12-12 16:17:44 +0530

    Updates

---
+++ precache-manifest.78456b65a661b790565ddc6da9316f5f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a00eaff4ecb1cd073c5548496e324f8",
+    "url": "/ookstudios/static/media/kalhan-mattoo.6a00eaff.png"
+  },
+  {
+    "revision": "d3cf8f1797eb4962af0f",
+    "url": "/ookstudios/static/css/main.61283c72.chunk.css"
+  },
+  {
+    "revision": "45dd367e34ea9ada035abeadbbcdbd58",
+    "url": "/ookstudios/static/media/augment.45dd367e.png"
+  },
+  {
+    "revision": "4ca6cf587d40b171a5f9",
+    "url": "/ookstudios/static/js/1.4ca6cf58.chunk.js"
+  },
+  {
+    "revision": "18df1fc58b92430f35f0",
+    "url": "/ookstudios/static/js/runtime~main.18df1fc5.js"
+  },
+  {
+    "revision": "487f5a6474c65b09a26676907bea45ec",
+    "url": "/ookstudios/static/media/small-o.487f5a64.png"
+  },
+  {
+    "revision": "b2c96e9b0d5a584dd24a5380205e2a5e",
+    "url": "/ookstudios/static/media/big-o.b2c96e9b.png"
+  },
+  {
+    "revision": "d3cf8f1797eb4962af0f",
+    "url": "/ookstudios/static/js/main.d3cf8f17.chunk.js"
+  },
+  {
+    "revision": "ab0fc04520c8ce7c737d07f63128602f",
+    "url": "/ookstudios/static/media/logo.ab0fc045.jpg"
+  },
+  {
+    "revision": "e3cb0c7fbc038b33c7ef015ca14775a8",
+    "url": "/ookstudios/static/media/planet-studio.e3cb0c7f.jpg"
+  },
+  {
+    "revision": "cdf690c27750ac709ac94b833c1a2737",
+    "url": "/ookstudios/static/media/one team.cdf690c2.png"
+  },
+  {
+    "revision": "585e56e44bc50f97ce9fa29fa47140fc",
+    "url": "/ookstudios/static/media/lab.585e56e4.png"
+  },
+  {
+    "revision": "4ca6cf587d40b171a5f9",
+    "url": "/ookstudios/static/css/1.95260b32.chunk.css"
+  },
+  {
+    "revision": "1a39a3748ee8b0624429454283c216ac",
+    "url": "/ookstudios/index.html"
+  }
+];
