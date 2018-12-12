0ec41d8a2f2708d484a70ecacc0ade235f3c7709
Ashutosh Telang <ashutoshtel@gmail.com>
2018-12-12 16:59:01 +0530

    nav fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 import "./css/logo.css";
 import "./css/animation.css";
